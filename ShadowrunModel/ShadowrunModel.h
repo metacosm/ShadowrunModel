2907a85b1be9d9e1b7ea1d5a31d01f5099a64dc1
Chris Laprun <metacosm@gmail.com>
2017-03-07 13:57:34 +0100

    Initial Commit

---
+++ ShadowrunModel/ShadowrunModel.h
@@ -0,0 +1,19 @@
+//
+//  ShadowrunModel.h
+//  ShadowrunModel
+//
+//  Created by Christophe Laprun on 07/03/2017.
+//  Copyright © 2017 Christophe Laprun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ShadowrunModel.
+FOUNDATION_EXPORT double ShadowrunModelVersionNumber;
+
+//! Project version string for ShadowrunModel.
+FOUNDATION_EXPORT const unsigned char ShadowrunModelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ShadowrunModel/PublicHeader.h>
+
+
